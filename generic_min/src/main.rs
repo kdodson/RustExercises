fc9b96f0626ae37838387475f8f1062e8b6e6f6c
Kyle Dodson <kylejdodson@gmail.com>
2025-07-14 19:02:32 -0500

    Added generic_min for learning generics like: - Generic functions - Trait Bounds - Generic Data Types

---
+++ generic_min/src/main.rs
@@ -0,0 +1,27 @@
+use std::cmp::Ordering;
+
+// TODO: implement the `min` function used in the tests.
+fn min<T: Ord>(lhs: T, rhs: T) -> T {
+    match lhs.cmp(&rhs) {
+        Ordering::Less | Ordering::Equal => lhs,
+        Ordering::Greater => rhs
+    }
+}
+
+#[test]
+fn integers() {
+    assert_eq!(min(0, 10), 0);
+    assert_eq!(min(500, 123), 123);
+}
+
+#[test]
+fn chars() {
+    assert_eq!(min('a', 'z'), 'a');
+    assert_eq!(min('7', '1'), '1');
+}
+
+#[test]
+fn strings() {
+    assert_eq!(min("hello", "goodbye"), "goodbye");
+    assert_eq!(min("bat", "armadillo"), "armadillo");
+}
